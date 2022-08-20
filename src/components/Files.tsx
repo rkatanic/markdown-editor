13323810841a59bb6f2cacf9546e303ace5bfd52
Radivoje Katanic <k.rade313@gmail.com>
2022-08-20 22:32:17 +0200

    Adds empty state

--- src/components/Files.tsx
+++ src/components/Files.tsx
@@ -1,5 +1,5 @@
 import { MarkdownFile } from "../types/markdown";
-import { FiTrash2, FiX } from "react-icons/fi";
+import { FiFilePlus, FiTrash2, FiX } from "react-icons/fi";
 
 interface Props {
   files: MarkdownFile[];
@@ -51,36 +51,49 @@ const Files = ({
             className="lg:hidden dark:stroke-zinc-400 stroke-zinc-500"
           />
         </div>
-        <div className="w-full max-w-sm">
-          {files.map(({ name, markdown }, i) => (
-            <div
-              key={i}
-              className={`${
-                currentFile?.name === name
-                  ? "bg-zinc-50 dark:bg-zinc-700/25"
-                  : ""
-              } border-b px-6 py-4 dark:border-zinc-700`}
-            >
-              <p className="cursor-pointer mb-2 font-semibold flex items-center gap-2 justify-between dark:text-zinc-200">
-                <span onClick={(): void => handleFilesSelect(name)}>
-                  {name}
-                </span>
-                <FiTrash2
-                  data-testid="trash-icon"
-                  onClick={() => deleteFile(name)}
-                  size="1.125rem"
-                  className="stroke-zinc-300 hover:stroke-rose-500 hover:cursor-pointer dark:stroke-zinc-500 dark:hover:stroke-rose-800"
-                />
-              </p>
-              <p
-                onClick={(): void => handleFilesSelect(name)}
-                className="cursor-pointer overflow-hidden text-ellipsis max-h-16 text-sm text-zinc-500 dark:text-zinc-400"
+        {files.length ? (
+          <div className="w-full max-w-sm">
+            {files.map(({ name, markdown }, i) => (
+              <div
+                key={i}
+                className={`${
+                  currentFile?.name === name
+                    ? "bg-zinc-50 dark:bg-zinc-700/25"
+                    : ""
+                } border-b px-6 py-4 dark:border-zinc-700`}
               >
-                {markdown}
-              </p>
+                <p className="cursor-pointer mb-2 font-semibold flex items-center gap-2 justify-between dark:text-zinc-200">
+                  <span onClick={(): void => handleFilesSelect(name)}>
+                    {name}
+                  </span>
+                  <FiTrash2
+                    data-testid="trash-icon"
+                    onClick={() => deleteFile(name)}
+                    size="1.125rem"
+                    className="stroke-zinc-300 hover:stroke-rose-500 hover:cursor-pointer dark:stroke-zinc-500 dark:hover:stroke-rose-800"
+                  />
+                </p>
+                <p
+                  onClick={(): void => handleFilesSelect(name)}
+                  className="cursor-pointer overflow-hidden text-ellipsis max-h-16 text-sm text-zinc-500 dark:text-zinc-400"
+                >
+                  {markdown}
+                </p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center gap-1 mt-8">
+            <FiFilePlus
+              size="2.5rem"
+              className="stroke-1 stroke-zinc-300 mb-1"
+            />
+            <div className="text-zinc-700 font-semibold">No notes saved</div>
+            <div className="text-sm text-zinc-400">
+              Get started by saving your first note.
             </div>
-          ))}
-        </div>
+          </div>
+        )}
       </div>
     </>
   );
